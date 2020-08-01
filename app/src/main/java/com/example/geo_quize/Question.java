a2139c9cdbd07faf7ec9cee7804aaa6f34da2f3c
MaryamBanytlebi <Banytalebi.m@gmail.com>
2020-08-01 11:34:02 -0700

    Initial commit

---
+++ app/src/main/java/com/example/geo_quize/Question.java
@@ -0,0 +1,43 @@
+package com.example.geo_quize;
+
+public class Question {
+    private int mQuestionResId;
+    private boolean mIsAnswerTrue;
+    private boolean mdisable;
+
+    public boolean isMdisable() {
+        return mdisable;
+    }
+
+    public void setMdisable(boolean mdisable) {
+        this.mdisable = mdisable;
+    }
+
+    public int getQuestionResId() {
+        return mQuestionResId;
+    }
+
+    public void setQuestionResId(int questionResId) {
+        mQuestionResId = questionResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mIsAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mIsAnswerTrue = answerTrue;
+    }
+
+    public Question(int questionResId, boolean isAnswerTrue) {
+        mQuestionResId = questionResId;
+        mIsAnswerTrue = isAnswerTrue;
+        mdisable=false;
+    }
+
+    public Question() {
+        mdisable=false;
+    }
+
+
+}
